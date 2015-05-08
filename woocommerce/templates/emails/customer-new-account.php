79562392970ab142ed9001916681f9f0d8d46cd4
Rob Benz <benz_rob@yahoo.com>
2015-05-08 09:29:20 -0400

    change from child theme to actual woocommerce email pages

--- woocommerce/templates/emails/customer-new-account.php
+++ woocommerce/templates/emails/customer-new-account.php
@@ -15,14 +15,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php do_action( 'woocommerce_email_header', $email_heading ); ?>
 
-<p><?php printf( __( "Thanks for creating an account on %s. Your username is <strong>%s</strong>.", 'woocommerce' ), esc_html( $blogname ), esc_html( $user_login ) ); ?></p>
+<p><?php printf( __( "Thanks for creating an account on %s.", 'woocommerce' ), esc_html( $blogname ), esc_html( $user_login ) ); ?></p>
 
 <?php if ( get_option( 'woocommerce_registration_generate_password' ) == 'yes' && $password_generated ) : ?>
 
-	<p><?php printf( __( "Your password has been automatically generated: <strong>%s</strong>", 'woocommerce' ), esc_html( $user_pass ) ); ?></p>
+	<p><?php //printf( __( "Your password has been automatically generated: <strong>%s</strong>", 'woocommerce' ), esc_html( $user_pass ) ); ?></p>
 
 <?php endif; ?>
 
 <p><?php printf( __( 'You can access your account area to view your orders and change your password here: %s.', 'woocommerce' ), wc_get_page_permalink( 'myaccount' ) ); ?></p>
+
+<p style="color:#00426a; font-size:22px;text-align:center;font-weight:bold;"><a href="https://partsonline.diamedicalusa.com/">SHOP NOW</a></p>
+
+<p style="color:#00426a; font-size:18px;"><a href="https://partsonline.diamedicalusa.com/">DiaMedical USA offers discounts on over 160,000 parts!</a></p>
 
 <?php do_action( 'woocommerce_email_footer' ); ?>
--- woocommerce/templates/emails/customer-reset-password.php
+++ woocommerce/templates/emails/customer-reset-password.php
@@ -15,14 +15,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <?php do_action( 'woocommerce_email_header', $email_heading ); ?>
 
-<p><?php _e( 'Someone requested that the password be reset for the following account:', 'woocommerce' ); ?></p>
-<p><?php printf( __( 'Username: %s', 'woocommerce' ), $user_login ); ?></p>
-<p><?php _e( 'If this was a mistake, just ignore this email and nothing will happen.', 'woocommerce' ); ?></p>
-<p><?php _e( 'To reset your password, visit the following address:', 'woocommerce' ); ?></p>
+<p><?php _e( 'Someone requested that the password be reset for the following DiaMedical USA | Parts Online account:', 'woocommerce' ); ?></p>
 <p>
     <a href="<?php echo esc_url( add_query_arg( array( 'key' => $reset_key, 'login' => rawurlencode( $user_login ) ), wc_get_endpoint_url( 'lost-password', '', wc_get_page_permalink( 'myaccount' ) ) ) ); ?>">
-			<?php _e( 'Click here to reset your password', 'woocommerce' ); ?></a>
+			<p style="color:#00426a; font-size:22px;text-align:center;font-weight:bold;"><?php _e( 'Click here to reset your password', 'woocommerce' ); ?></p></a>
 </p>
+
+<p><?php// printf( __( 'Username: %s', 'woocommerce' ), $user_login ); ?></p>
+
+<p><?php// _e( 'To reset your password, visit the following address:', 'woocommerce' ); ?></p>
+
 <p></p>
+<p><?php _e( 'If this was a mistake, just ignore this email and nothing will happen.', 'woocommerce' ); ?></p>
 
 <?php do_action( 'woocommerce_email_footer' ); ?>
