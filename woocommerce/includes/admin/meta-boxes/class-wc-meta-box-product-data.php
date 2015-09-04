1e358f785290c01f7336c247822e3afd20219cd9
Rob Benz <benz_rob@yahoo.com>
2015-09-04 11:06:11 -0400

    change error message

--- woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-data.php
+++ woocommerce/includes/admin/meta-boxes/class-wc-meta-box-product-data.php
@@ -1411,8 +1411,8 @@ class WC_Meta_Box_Product_Data {
 					if ( ! empty( $new_sku ) ) {
 						$unique_sku = wc_product_has_unique_sku( $variation_id, $new_sku );
 
-						if ( ! $unique_sku ) {
-							WC_Admin_Meta_Boxes::add_error( __( 'HAHA You SUCK AT WORDPRESS!!!', 'woocommerce' ) );
+						if ( ! $unique_sku ) 
+							WC_Admin_Meta_Boxes::add_error( __( 'Your SKUs are Fucked up again.', 'woocommerce' ) );
 						} else {
 							update_post_meta( $variation_id, '_sku', $new_sku );
 						}
