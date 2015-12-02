cdf1e276e4e45a395e169b773443a0da94104d73
Rob Benz <benz_rob@yahoo.com>
2015-12-02 11:13:15 -0500

    update to link checker plugin

--- link-checker/linkchecker.php
+++ link-checker/linkchecker.php
@@ -4,12 +4,13 @@
  * @copyright  Copyright (C) 2015 Marco Beierer. All rights reserved.
  * @license    https://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
  */
+defined('ABSPATH') or die('Restricted access.');
 
 /*
 Plugin Name: Link Checker
 Plugin URI: https://www.marcobeierer.com/wordpress-plugins/link-checker
-Description: An easy to use Link Checker for WordPress to detect broken internal and external links on your website.
-Version: 1.0.0-beta.2
+Description: An easy to use Link Checker for WordPress to detect broken links and images on your website.
+Version: 1.1.3
 Author: Marco Beierer
 Author URI: https://www.marcobeierer.com
 License: GPL v3
@@ -22,49 +23,65 @@ function register_link_checker_page() {
 }
 
 function link_checker_page() {
-?>
+	include_once('shared_functions.php'); ?>
+
 	<div class="wrap" id="linkchecker-widget" ng-app="linkCheckerApp" ng-strict-di>
 		<div ng-controller="LinkCheckerController">
 			<form name="linkCheckerForm">
 				<h2>Link Checker <button type="submit" class="add-new-h2" ng-click="check()" ng-disabled="checkDisabled">Check your website</button></h2>
 			</form>
-			<h3>Check your website for broken internal and external links.</h3>
-			<p>{{ message }} <span ng-if="urlsCrawledCount > 0">{{ urlsCrawledCount }} URLs already checked.</span> <span ng-if="limitReached">The URL limit was reached. The crawler has not checked your complete website.</span></p>
 
-			<table class="wp-list-table widefat fixed striped posts">
-				<thead>
-					<tr>
-						<th style="width: 35%;">URL where the broken link was found</th>
-						<th>Broken URL</th>
-						<th style="width: 6em;">Status Code</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr ng-if="!links">
-						<td>No broken links found yet.</td>
-						<td></td>
-						<td></td>
-					</tr>
-					<tr ng-repeat="(foundOnURL, deadLinks) in links">
-						<td><a href="{{ foundOnURL }}">{{ foundOnURL }}</a></td>
-						<td colspan="2">
-							<table class="wp-list-table widefat fixed">
-								<tr ng-repeat="deadLink in deadLinks">
-									<td>{{ deadLink.URL }}</td>
-									<td style="width: 5em;">{{ deadLink.StatusCode }}</td>
-								</tr>
-							</table>
-						</td>
-					</tr>
-				</tbody>
-				<tfoot>
-					<tr>
-						<th>URL where the broken link was found</th>
-						<th>Broken URL</th>
-						<th>Status Code</th>
-					</tr>
-				</tfoot>
+			<?php
+				cURLCheck();
+				localhostCheck();
+			?>
+
+			<h3>Check your website for broken internal and external links.</h3>
+			<p ng-bind-html="message | sanitize"></p>
+
+			<table>
+				<tr>
+					<td>Number of crawled HTML pages on your site:</td>
+					<td>{{ urlsCrawledCount }}</td>
+				</tr>
+				<tr>
+					<td>Number of checked internal and external resources:</td>
+					<td>{{ checkedLinksCount }}</td>
+				</tr>
 			</table>
+
+			<h3>Broken Links</h3>
+			<?php
+				include_once('template.php');
+
+				$templateFilepath = plugin_dir_path(__FILE__) . 'tmpl/table.html';
+				$template = new MarcoBeierer\Template($templateFilepath);
+
+				$template->setVar('th-col1', 'URL where the broken links were found');
+				$template->setVar('th-col2', 'Broken Links');
+				$template->setVar('th-col3', 'Status Code');
+				$template->setVar('list', 'links');
+
+				$template->render();
+			?>
+
+			<?php
+				$token = get_option('link-checker-token');
+				if ($token != ''): 
+			?>
+			<h3>Broken Images</h3>
+			<?php
+				$template = new MarcoBeierer\Template($templateFilepath);
+
+				$template->setVar('th-col1', 'URL where the broken images were found');
+				$template->setVar('th-col2', 'Broken Images');
+				$template->setVar('th-col3', 'Status Code');
+				$template->setVar('list', 'urlsWithDeadImages');
+
+				$template->render();
+
+				endif; 
+			?>
 		</div>
 	</div>
 <?
@@ -76,7 +93,7 @@ function load_link_checker_admin_scripts($hook) {
 	if ($hook == 'toplevel_page_link-checker') {
 
 		$angularURL = plugins_url('js/angular.min.js', __FILE__);
-		$linkcheckerURL = plugins_url('js/linkchecker.js?v=1', __FILE__);
+		$linkcheckerURL = plugins_url('js/linkchecker.js?v=6', __FILE__);
 
 		wp_enqueue_script('link_checker_angularjs', $angularURL);
 		wp_enqueue_script('link_checker_linkcheckerjs', $linkcheckerURL);
@@ -95,10 +112,20 @@ function link_checker_proxy_callback() {
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
+	$token = get_option('link-checker-token');
+	if ($token != '') {
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: BEARER ' . $token));
+		header('X-Used-Token: 1');
+	}
+
 	$response = curl_exec($ch);
 
 	$statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 	$contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+	if ($statusCode == 0) {
+		$statusCode = 503; // Service unavailable
+	}
 
 	curl_close($ch);
 
@@ -115,3 +142,33 @@ function link_checker_proxy_callback() {
 	echo $response;
 	wp_die();
 }
+
+add_action('admin_menu', 'register_link_checker_settings_page');
+function register_link_checker_settings_page() {
+	add_submenu_page('link-checker', 'Link Checker Settings', 'Settings', 'manage_options', 'link-checker-settings', 'link_checker_settings_page');
+	add_action('admin_init', 'register_link_checker_settings');
+}
+
+function register_link_checker_settings() {
+	register_setting('link-checker-settings-group', 'link-checker-token');
+}
+
+function link_checker_settings_page() {
+?>
+	<div class="wrap">
+		<h2>Link Checker Settings</h2>
+		<div class="card">
+			<form method="post" action="options.php">
+				<?php settings_fields('link-checker-settings-group'); ?>
+				<?php do_settings_sections('link-checker-settings-group'); ?>
+				<h3>Your Token</h3>
+				<p><textarea name="link-checker-token" style="width: 100%; min-height: 350px;"><?php echo esc_attr(get_option('link-checker-token')); ?></textarea></p>
+				<p>The Link Checker allows you to check up to 500 internal and external links for free. If your website has more links, you can buy a token for the <a href="https://www.marcobeierer.com/wordpress-plugins/link-checker-professional">Link Checker Professional</a> to check up to 50'000 links.</p>
+				<p>The professional version also checks if you have broken embedded images on your site.</p>
+				<?php submit_button(); ?>
+			</form>
+		</div>
+	</div>
+<?php
+}
+?>
