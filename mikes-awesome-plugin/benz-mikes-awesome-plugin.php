5acddac49e848b33e4a65918daca5f9b1ea23094
Rob Benz <benz_rob@yahoo.com>
2015-12-08 14:19:10 -0500

    push first benz plugin

---
+++ mikes-awesome-plugin/benz-mikes-awesome-plugin.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Plugin Name: Mike's Awesome Green Bar Plugin 
+ * Plugin URI: http://www.robbenz.com
+ * Description: This Plugin is for Mike Kulig and Mike Kulig ONLY! This is so he can change the Promotion on the Green Bar up top 
+ * Version: 1.0
+ * Author: RobBenz
+ * Author URI: http://www.robbenz.com 
+ * License: GPL2
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+?>
