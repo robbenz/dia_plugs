d2d4e78c04f0586d1a5545253fc6961160267e29
Rob Benz <benz_rob@yahoo.com>
2017-02-28 08:21:06 -0500

    plugin dia tabs update sql tables

--- dia-wc-product-tabs/dia-tabs-admin.php
+++ dia-wc-product-tabs/dia-tabs-admin.php
@@ -12,24 +12,24 @@ add_action("add_meta_boxes", "add_dia_meta_box");
 function dia_meta_box_markup($object) {
   wp_nonce_field(basename(__FILE__), "meta-box-nonce");
   	global $post;
-    $dia_tab_count = get_post_meta( $post->ID, '_wcj_custom_product_tabs_local_total_number', true );
+    $dia_tab_count = get_post_meta( $post->ID, '_dia_tabs_local_total_number', true );
   ?>
 
-  <label for="_wcj_custom_product_tabs_local_total_number"><?php echo __( 'How Many Tabs', 'woocommerce' ); ?></label>
-  <input name="_wcj_custom_product_tabs_local_total_number" class="" type="number" name=" " value="<?php echo $dia_tab_count; ?>" step="any" min="0" max="5" style="width: 50px;" />
+  <label for="_dia_tabs_local_total_number"><?php echo __( 'How Many Tabs', 'woocommerce' ); ?></label>
+  <input name="_dia_tabs_local_total_number" class="" type="number" name=" " value="<?php echo $dia_tab_count; ?>" step="any" min="0" max="5" style="width: 50px;" />
   <br />
 
 <?php for ( $x = 0; $x < $dia_tab_count; $x++ ) {
   $y=$x+1;
-  $dia_tab_title = get_post_meta( $post->ID, "_wcj_custom_product_tabs_title_local_$y", true );
-  $dia_tab_content = get_post_meta( $post->ID, "_wcj_custom_product_tabs_content_local_$y", true );
+  $dia_tab_title = get_post_meta( $post->ID, "_dia_tabs_title_local_$y", true );
+  $dia_tab_content = get_post_meta( $post->ID, "_dia_tabs_content_local_$y", true );
 ?>
   <div>
-    <label for="_wcj_custom_product_tabs_title_local_<?php echo $y; ?>">Tab <?php echo $y ;?> Heading: </label>
-    <input name="_wcj_custom_product_tabs_title_local_<?php echo $y; ?>" type="text" value="<?php echo $dia_tab_title; ?>">
+    <label for="_dia_tabs_title_local_<?php echo $y; ?>">Tab <?php echo $y ;?> Heading: </label>
+    <input name="_dia_tabs_title_local_<?php echo $y; ?>" type="text" value="<?php echo $dia_tab_title; ?>">
     <br>
     <?php
-        $dia_tab_content = get_post_meta( $post->ID, "_wcj_custom_product_tabs_content_local_$y", true );
+        $dia_tab_content = get_post_meta( $post->ID, "_dia_tabs_content_local_$y", true );
         	if ( ! $dia_tab_content ) {
         		$dia_tab_content = '';
         	}
@@ -66,33 +66,33 @@ function save_custom_meta_box($post_id, $post, $update) {
 
     // Tab Count
     $meta_box_tab_count_value = "";
-    if(isset($_POST["_wcj_custom_product_tabs_local_total_number"])) {
-        $meta_box_tab_count_value = $_POST["_wcj_custom_product_tabs_local_total_number"];
+    if(isset($_POST["_dia_tabs_local_total_number"])) {
+        $meta_box_tab_count_value = $_POST["_dia_tabs_local_total_number"];
     }
-    update_post_meta($post_id, "_wcj_custom_product_tabs_local_total_number", $meta_box_tab_count_value);
+    update_post_meta($post_id, "_dia_tabs_local_total_number", $meta_box_tab_count_value);
 
     global $post;
-    $dia_tab_count = get_post_meta( $post->ID, '_wcj_custom_product_tabs_local_total_number', true );
+    $dia_tab_count = get_post_meta( $post->ID, '_dia_tabs_local_total_number', true );
     for ( $x = 0; $x < $dia_tab_count; $x++ ) {
       $y=$x+1;
 
       // DYNAMICALLY SAVE TAB TITLES - PER TAB COUNT
       $meta_box_tab_tab_value = "";
-      if(isset($_POST["_wcj_custom_product_tabs_title_local_$y"])) {
-        $meta_box_tab_tab_value = $_POST["_wcj_custom_product_tabs_title_local_$y"];
+      if(isset($_POST["_dia_tabs_title_local_$y"])) {
+        $meta_box_tab_tab_value = $_POST["_dia_tabs_title_local_$y"];
       }
-      update_post_meta($post_id, "_wcj_custom_product_tabs_title_local_$y", $meta_box_tab_tab_value);
+      update_post_meta($post_id, "_dia_tabs_title_local_$y", $meta_box_tab_tab_value);
 
       // DYNAMICALLY SAVE THE STUFF IN THE TAB CONTENTS BOX
       if ( ! isset( $_POST["dia_product_tabs_details_nonce_$y"] ) || ! wp_verify_nonce( $_POST["dia_product_tabs_details_nonce_$y"], basename( __FILE__ ) ) ) {
         return;
       }
-      $old_details = get_post_meta( $post->ID, "_wcj_custom_product_tabs_content_local_1", true );
+      $old_details = get_post_meta( $post->ID, "_dia_tabs_content_local_1", true );
       $new_details = isset( $_POST["dia-product-tabs-details_$y"] ) ? $_POST["dia-product-tabs-details_$y"] : '';
       if ( $old_details && '' === $new_details ) {
-        delete_post_meta( $post_id, "_wcj_custom_product_tabs_content_local_$y" );
+        delete_post_meta( $post_id, "_dia_tabs_content_local_$y" );
       } else if ( $old_details !== $new_details ) {
-        update_post_meta( $post_id, "_wcj_custom_product_tabs_content_local_$y", wp_kses_post( $new_details ) );
+        update_post_meta( $post_id, "_dia_tabs_content_local_$y", wp_kses_post( $new_details ) );
       }
     } // end for loop
 
--- dia-wc-product-tabs/dia-tabs-frontend.php
+++ dia-wc-product-tabs/dia-tabs-frontend.php
@@ -6,19 +6,19 @@ add_filter( 'woocommerce_product_tabs', 'dia_new_product_tabs' );
 /*** ADD TABS & TAB TITLES ***/
 function dia_new_product_tabs( $tabs ) {
   global $post;
-  $dia_tab_count = get_post_meta( $post->ID, '_wcj_custom_product_tabs_local_total_number', true );
+  $dia_tab_count = get_post_meta( $post->ID, '_dia_tabs_local_total_number', true );
   for ( $x = 0; $x < $dia_tab_count; $x++ ) {
     $y=$x+1;
-    $dia_tab_title = get_post_meta( $post->ID, "_wcj_custom_product_tabs_title_local_$y", true );
+    $dia_tab_title = get_post_meta( $post->ID, "_dia_tabs_title_local_$y", true );
     $dia_tab_title_clean = preg_replace('/\s+/', '-', $dia_tab_title);
-    $dia_tab_content = get_post_meta( $post->ID, "_wcj_custom_product_tabs_content_local_$y", true );
+    $dia_tab_content = get_post_meta( $post->ID, "_dia_tabs_content_local_$y", true );
 
     if ( strlen($dia_tab_title) > 0 && strlen($dia_tab_content) > 0 ) {
       $tabs[$dia_tab_title_clean] = array(
         'title'   	=> __( $dia_tab_title, 'woocommerce' ),
         'priority' 	=> $y+50,
         'callback' 	=> 'dia_new_product_tab_content',
-        'args'      => '_wcj_custom_product_tabs_content_local_' . $y
+        'args'      => '_dia_tabs_content_local_' . $y
       );
     } // end foreach
   }
