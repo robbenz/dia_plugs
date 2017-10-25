7a063a477a9516e808b2ee069e977ab1ebf9f93e
Rob Benz <benz_rob@yahoo.com>
2017-10-25 12:01:21 -0400

    before crazy merge

--- search-by-sku-for-woocommerce/wc-searchbysku-widget-compat.php
+++ search-by-sku-for-woocommerce/wc-searchbysku-widget-compat.php
@@ -8,15 +8,15 @@ function product_search_sku($where) {
     global $pagenow, $wpdb, $wp;
     //VAR_DUMP(http_build_query(array('post_type' => array('product','boobs'))));die();
     $type = array('product', 'jam');
-    
-    
+
+
     //var_dump(in_array('product', $wp->query_vars['post_type']));
-    if ((is_admin() && 'edit.php' != $pagenow) 
-            || !is_search()  
-            || !isset($wp->query_vars['s']) 
+    if ((is_admin() && 'edit.php' != $pagenow)
+            || !is_search()
+            || !isset($wp->query_vars['s'])
             //post_types can also be arrays..
             || (isset($wp->query_vars['post_type']) && 'product' != $wp->query_vars['post_type'])
-            || (isset($wp->query_vars['post_type']) && is_array($wp->query_vars['post_type']) && !in_array('product', $wp->query_vars['post_type']) ) 
+            || (isset($wp->query_vars['post_type']) && is_array($wp->query_vars['post_type']) && !in_array('product', $wp->query_vars['post_type']) )
             ) {
         return $where;
     }
@@ -34,6 +34,7 @@ function product_search_sku($where) {
 
         //Search for a regular product that matches the sku.
         $sku_to_id = $wpdb->get_col($wpdb->prepare("SELECT post_id FROM {$wpdb->postmeta} WHERE meta_key='_sku' AND meta_value LIKE '%%%s%%';", wc_clean($term)));
+        //$dia_specs_to_id = $wpdb->get_col($wpdb->prepare("SELECT post_id FROM {$wpdb->postmeta} WHERE meta_key='dia_search_extra_terms' AND meta_value LIKE '%%%s%%';", wc_clean($term)));
 
         $search_ids = array_merge($search_ids, $sku_to_id, $sku_to_parent_id);
     }
@@ -43,7 +44,7 @@ function product_search_sku($where) {
     if (sizeof($search_ids) > 0) {
         $where = str_replace(')))', ") OR ({$wpdb->posts}.ID IN (" . implode(',', $search_ids) . "))))", $where);
     }
-    
+
     remove_filters_for_anonymous_class('posts_search', 'WC_Admin_Post_Types', 'product_search', 10);
     return $where;
 }
