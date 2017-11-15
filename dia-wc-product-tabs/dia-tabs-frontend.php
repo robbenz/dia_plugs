1241521db3453812f275968638bc454f2af5ff64
Rob Benz <benz_rob@yahoo.com>
2017-11-15 14:00:23 -0500

    js changed and str replace in tabs.php

--- dia-wc-product-tabs/dia-tabs-frontend.php
+++ dia-wc-product-tabs/dia-tabs-frontend.php
@@ -6,18 +6,17 @@ add_filter( 'woocommerce_product_tabs', 'dia_new_product_tabs' );
 function dia_new_product_tabs( $tabs ) {
   global $post;
   $dia_tab_count = get_post_meta( $post->ID, '_dia_tabs_local_total_number', true );
-  for ( $x = 0; $x < $dia_tab_count; $x++ ) {
-    $y=$x+1;
-    $dia_tab_title = get_post_meta( $post->ID, "_dia_tabs_title_local_$y", true );
+  for ( $x = 1; $x < $dia_tab_count; $x++ ) {
+    $dia_tab_title = get_post_meta( $post->ID, "_dia_tabs_title_local_$x", true );
     $dia_tab_title_clean = preg_replace('/\s+/', '-', $dia_tab_title);
-    $dia_tab_content = get_post_meta( $post->ID, "_dia_tabs_content_local_$y", true );
+    $dia_tab_content = get_post_meta( $post->ID, "_dia_tabs_content_local_$x", true );
 
     if ( strlen($dia_tab_title) > 0 && strlen($dia_tab_content) > 0 ) {
       $tabs[$dia_tab_title_clean] = array(
         'title'   	=> __( $dia_tab_title, 'woocommerce' ),
-        'priority' 	=> $y+50,
+        'priority' 	=> $x+50,
         'callback' 	=> 'dia_new_product_tab_content',
-        'args'      => '_dia_tabs_content_local_' . $y
+        'args'      => '_dia_tabs_content_local_' . $x
       );
     } // end foreach
   }
@@ -37,7 +36,8 @@ function dia_new_product_tab_content($param, $args) {
   if (0 === strpos($dia_tab_content, '<h2>')) {
     print_r ($dia_tab_content);
   } else {
-    echo '<h2>'.$param.'</h2>';
+    $param_clean = str_replace('-', ' ', $param);
+    echo '<h2>'.$param_clean.'</h2>';
     print_r ($dia_tab_content);
   }
 }
